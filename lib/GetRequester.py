476f7457451d99e914fe629aaa87030286fd61e8
Sharon Mwende <sharonndisya2002@gmail.com>
2024-04-13 13:36:15 +0300

    completed

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -3,11 +3,14 @@ import json
 
 class GetRequester:
 
+
     def __init__(self, url):
         self.url = url
 
-    def get_response_body(self):
-        pass
+    def get_response_body(self): 
+        response = requests.get(self.url)
+        return response.text 
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        return json.loads(response_body)  
